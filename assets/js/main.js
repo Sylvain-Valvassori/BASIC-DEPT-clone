2fdc926e80b863fcef3854aa4b83a6bae329209f
Sylvain-Valvassori <89160536+Sylvain-Valvassori@users.noreply.github.com>
2023-02-08 23:26:23 +0100

    touch mobile and screen done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,40 @@
 const slider = document.querySelector('.clients__slider__wrapper');
 const sliderBox = document.querySelector('.clients__slider__wrapper__box');
 const scrollBar = document.querySelector('.clients__slider__scroll-bar');
-
 const cursorScroll = document.getElementById('cursorScroll');
 
 let isDown = false;
-let sliderWidth, sliderInitialPosX, mouseInitialPosX, cursorInitialPosX, moveCursorValue, cursorWidth, scrollBarWidth, sliderBoxWidth;
+let initialPositionX, positionX, sliderInitialPositionX, moveCursorValue;
+let sliderWidth, cursorWidth, scrollBarWidth, sliderBoxWidth;
 
 
 const notActive = () => {
     isDown = false;
 }
 
-const getSliderInfo = (e) => {
+const getAllSliderInfo = (e) => {
+
     e.preventDefault();
     isDown = true;
-    sliderWidth         = slider.clientWidth;
-    sliderBoxWidth      = sliderBox.clientWidth;
-    sliderInitialPosX   = slider.scrollLeft;
-    mouseInitialPosX    = e.clientX;
-    
+    sliderWidth = slider.clientWidth;
+    sliderBoxWidth = sliderBox.clientWidth;
+    sliderInitialPositionX = slider.scrollLeft;
     cursorWidth =  cursorScroll.getBoundingClientRect().width;
     scrollBarWidth =  scrollBar.getBoundingClientRect().width;
+    
+    initialPositionX = e.clientX;
+    if(e.type == 'touchstart') initialPositionX = e.touches[0].clientX;
 }
 
-
 const moveSlider = (e) => {
+
     if (!isDown) return;
     e.preventDefault();
-    const mousePosX = e.clientX;
-    const moveSlider = sliderInitialPosX - (mousePosX - mouseInitialPosX);
+
+    positionX = e.clientX;
+    if(e.type == 'touchmove') positionX = e.touches[0].clientX;
+    
+    const moveSlider = sliderInitialPositionX - (positionX - initialPositionX);
     slider.scrollLeft = moveSlider;
 
     moveCursor(moveSlider); 
@@ -37,29 +42,30 @@ const moveSlider = (e) => {
 
 const moveCursor = (moveSlider) => {
 
-    const gapSlider = scrollBarWidth - cursorWidth;
-    const gapScrollBar = sliderBoxWidth - sliderWidth;
-
-    const ratioSlider =  gapSlider / gapScrollBar;
+    const gapSlider     = scrollBarWidth - cursorWidth;
+    const gapScrollBar  = sliderBoxWidth - sliderWidth;
+    const ratioSlider   = gapSlider / gapScrollBar;
     
-
     moveCursorValue = moveSlider * ratioSlider;
 
-    condition1 = moveCursorValue <= 0; 
-    condition2 = (cursorWidth + moveCursorValue) >=  scrollBarWidth; 
+    const condition1 = moveCursorValue <= 0; 
+    const condition2 = (cursorWidth + moveCursorValue) >=  scrollBarWidth; 
 
     condition1 ? moveCursorValue = 0 : 
-    condition2 ? moveCursorValue = scrollBarWidth - cursorWidth : 
-    moveCursorValue;
-
+    condition2 ? moveCursorValue = gapSlider : moveCursorValue;
     moveCursorValue;
 
     return cursorScroll.style.transform = 'translateX( '+ moveCursorValue +'px )';
 }
 
 
-
-slider.addEventListener('mousedown' , getSliderInfo);
-slider.addEventListener('mouseleave', notActive);
-slider.addEventListener('mouseup'   , notActive);
+slider.addEventListener('mousedown' , getAllSliderInfo);
 slider.addEventListener('mousemove' , moveSlider);
+slider.addEventListener('mouseup'   , notActive);
+slider.addEventListener('mouseleave', notActive);
+// mobile event
+slider.addEventListener('touchstart' , getAllSliderInfo, false);
+slider.addEventListener('touchmove'  , moveSlider, false);
+slider.addEventListener('touchend'   , notActive, false);
+
+
